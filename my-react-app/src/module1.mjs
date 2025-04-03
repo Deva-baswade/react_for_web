bbbaf4f421bab13eed3f76cc3fd9a0ca532b23c7
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-04-03 22:55:49 +0530

    this the for checking and learn error

---
+++ my-react-app/src/module1.mjs
@@ -0,0 +1,9 @@
+import dza, {a,b,c}from './module2.mjs';
+
+console.log(dza);
+console.log(a);
+
+
+console.log(b);
+console.log(c);
+
---
+++ my-react-app/src/module2.mjs
@@ -0,0 +1,11 @@
+const a = "ram";
+const b = "aditya";
+const c = "devanand";
+const d = "nagesh";
+const e = "pavan";
+
+export default e;
+export {a};
+export {b};
+export {c};
+
