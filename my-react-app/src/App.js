1eb5b0ebc6c7143c03b9fe0e09f0e706d2eecdb3
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-04-08 07:32:39 +0530

    this we add one alert button tag

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,28 +2,54 @@ import React, { useState } from 'react';
 import './App.css';
 import Navbar from './componant/Navbar';
 import First from './componant/First';
+import Alert from './componant/Alert';
 
 function App() {
   const [mode, setMode] = useState("white");
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+      
+  }
+
+
+
+
+
 
   const togglemode = () => {
     if (mode === "light") {
       setMode("white");
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
+      showAlert("the light mode enabled","success");
     } else {
       setMode("light");
-      document.body.style.backgroundColor = "black";
+      document.body.style.backgroundColor = "#222761";
       document.body.style.color = "white";
+      showAlert("the dark mode enabled","success");
     }
   };
+
+
+
+
   return(
     <>
       <Navbar mode={mode} togglemode={togglemode} />
+      <Alert alert={alert}/>
       <div className="container my-5">
         <h1>Enter the text</h1>
-        <First mode={mode} togglemode={togglemode} />
+        <First  showAlert={showAlert} mode={mode} togglemode={togglemode} />
       </div>
+    
     </>
   );
 }
--- my-react-app/src/componant/First.js
+++ my-react-app/src/componant/First.js
@@ -3,24 +3,29 @@ import React,{useState} from 'react'
 export default function First(props) {
   const handleclick = ()=>{   
     let usetext = text.toUpperCase()
-    settext(usetext)   
+    settext(usetext) 
+    props.showAlert("Text in upeer case","success");  
   }
   const handleclick1 = ()=>{   
     let usetext = text.toLowerCase()
-    settext(usetext)   
+    settext(usetext)
+    props.showAlert("Text in lower case","success");   
   }
   const handleclearclick = ()=>{
     settext("");
+    props.showAlert("You are clear text","success");
   }
   const handlechange1click = ()=>{
     const body = document.body;
     body.style.backgroundColor = "black"
     body.style.color = "white"
+    props.showAlert("the dark mode enabled","success");
   }
   const handlechangeclick = ()=>{
     const body = document.body;
     body.style.backgroundColor = "white"
     body.style.color = "black"
+    props.showAlert("the light mode enabled","success");
   }
   const handleOnChange = (event)=>{     
     settext(event.target.value)    
@@ -30,7 +35,7 @@ export default function First(props) {
     <>    
      <div className="box1">
     <h1>{props.h1}</h1>   
-    <textarea   type="text"  className="form-control"  id="exampleInputEmail1"  value={text}  onChange={handleOnChange}  style={{ width: '900px', height: '200px' }}  aria-describedby="emailHelp" mode={props.mode} togglemode={props.togglemode} />    
+    <textarea   type="text"  className={`form-control bg-${props.mode}`} id="exampleInputEmail1"  value={text}  onChange={handleOnChange}  style={{ width: '900px', height: '200px' }}  aria-describedby="emailHelp" />    
     <button type="submit" className="btn btn-primary mx-3" onClick={handleclick} >Change to upperCase</button>
     <button type="submit" className="btn btn-primary" onClick={handleclick1} >Change to upperCase</button>
     <button type="submit" className="btn btn-primary mx-3" onClick={handleclearclick} >Clear the textarea</button>
@@ -43,7 +48,7 @@ export default function First(props) {
       <p>{0.008 * text.split(" ").length }  minute for reading</p>
       <h2>Preview</h2>
       <p>{text}</p>
-    </div>
+      </div>
     </>
     
   )
